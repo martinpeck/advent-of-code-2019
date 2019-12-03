8f0dca48749430c32c1331ac6ecd78921b474212
Martin Peck <mpeck@microsoft.com>
2019-12-03 11:43:24 +0000

    add basic template for each day

---
+++ dayX/dayX.py
@@ -0,0 +1,20 @@
+def do_stuff():
+    raise NotImplementedError("Write some code, dude!")
+
+
+def solve_part1():
+    do_stuff()
+
+
+def solve_part2():
+    do_stuff()
+
+
+def solve_all():
+    print(f"*** {__file__} ***")
+    print(f"Solution to part 1: {solve_part1()}")
+    print(f"Solution to part 2: {solve_part2()}")
+
+
+if __name__ == "__main__":
+    solve_all()
---
+++ dayX/test_main.py
@@ -0,0 +1,7 @@
+import dayX
+import unittest
+
+
+class DayXTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, dayX.do_stuff())
